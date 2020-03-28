ce8981639a03c44a33be2d64b310be3fcc45f852
hyunsooseol <snow@cau.ac.kr>
2020-03-28 17:33:27 +0900

    add winPB

--- R/infitConfi.R
+++ R/infitConfi.R
@@ -20,6 +20,11 @@ infit.conf <- function(object){
   boot.infit<- boot.infit(object)
   result=c()
 
+  ## progress bar-------------------------------------------------------------------------------
+  winPB <- winProgressBar(title="bootstrap fit statistics", max=1000)
+  ##-----------------------------------------------------------------------------------------------
+
+
   for (i in 1:ncol(object)) {
 
     ci=boot::boot.ci(boot.infit, index = i,
@@ -27,6 +32,15 @@ infit.conf <- function(object){
     result=c(result, ci)
 
   }
+
+  ## progress bar----------------------------------------------------------------------------
+
+  for(i in 1:1000) {
+    Sys.sleep(0.01)
+    setWinProgressBar(winPB,i, title=paste(round(i/100*100,1), "% done"))
+  }
+close(winPB)
+
   return(result)
 }
 
